eac7d0ae15c5c21981c745c62db60162ffa36f81
IAN FLANAGAN <18FLANAGANI@huronvalley.org>
2018-05-21 09:06:00 -0400

    dancy boi

--- despacito.py
+++ despacito.py
@@ -13,13 +13,26 @@ while line:
 		print("comment")
 		line = fp.readline()
 		continue
-	print(line)
-	finch.led(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+	left = random.uniform(-0.5, 0.5)
+	right = random.uniform(-0.5, 0.5)
+	print("L:" + str(left), "R:" + str(right))
+	finch.wheels(left, right)
+
+	red = random.randint(0, 255)
+	green = random.randint(0, 255)
+	blue = random.randint(0, 255)
+	print(red, green, blue)
+	finch.led(red, green, blue)
+
+
 	line.replace(" ", "")
 
 	things = line.split(",")
 	if things[0] == "T" : finch.buzzer_with_delay(int(float(things[2]))/1000, int(float(things[1])))
 	if things[0] == "D" : sleep(int(float(things[1]))/1000)
+
+	print(line)
 	line=fp.readline()
 
 
